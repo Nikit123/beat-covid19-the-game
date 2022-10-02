273c4f0ac275ca77d4c6e2a5ff0fd0446c8b2003
Nikit_Jain <nikitjain19@gmail.com>
2022-10-02 17:55:33 +0530

    Updates

---
+++ precache-manifest.09895dcb52ce41a6faad7509687ddc1b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29981eab99744f02ecfb728a529ea051",
+    "url": "/Beat-Covid19-The-Game/index.html"
+  },
+  {
+    "revision": "f5102c22fdc9b95eb7e6",
+    "url": "/Beat-Covid19-The-Game/static/css/main.c284dc7a.chunk.css"
+  },
+  {
+    "revision": "9e5255749bd1ec8388fc",
+    "url": "/Beat-Covid19-The-Game/static/js/2.89289bbb.chunk.js"
+  },
+  {
+    "revision": "c1bdf306936ddd82567a345834b1f9d5",
+    "url": "/Beat-Covid19-The-Game/static/js/2.89289bbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5102c22fdc9b95eb7e6",
+    "url": "/Beat-Covid19-The-Game/static/js/main.46f5b6fb.chunk.js"
+  },
+  {
+    "revision": "1cd073cae7680d7dcbc2",
+    "url": "/Beat-Covid19-The-Game/static/js/runtime-main.1df968c5.js"
+  },
+  {
+    "revision": "2c352da4ffbebe163a09e4d721552886",
+    "url": "/Beat-Covid19-The-Game/static/media/covid.2c352da4.svg"
+  },
+  {
+    "revision": "5bde68a7e4fba2a23ff130eeec2061f7",
+    "url": "/Beat-Covid19-The-Game/static/media/globe.5bde68a7.png"
+  },
+  {
+    "revision": "3b693303ec6d8b5e44a0890612676ec9",
+    "url": "/Beat-Covid19-The-Game/static/media/lab.3b693303.png"
+  },
+  {
+    "revision": "ab4c37e8145f7d31cea381633ccad434",
+    "url": "/Beat-Covid19-The-Game/static/media/moving_covid.ab4c37e8.png"
+  }
+]);
